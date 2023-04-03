725dc3c0c69c8e9dbff4b69ae7cea3103b3736b1
Hibara-so-cool <beginnervarin2021@gmail.com>
2023-04-03 22:12:35 +0700

    change croissant name

--- lib/data/cake_old.dart
+++ lib/data/cake_old.dart
@@ -75,7 +75,7 @@
 //   ),
 //   Cake(
 //     id: 8,
-//     name: 'Milk Croissant',
+//     name: 'Plain Croissant',
 //     price: '69.50',
 //     imageUrl: 'assets/crois1.png',
 //     isFavorite: false,
